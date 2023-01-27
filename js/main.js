57de21c214f7152a604afeb536b8bfb9e86af648
Adam Girzishev <girzishev99@bk.ru>
2023-01-27 16:31:07 +0300

    progress
    progress

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*function func(x, y) {
+  console.log("Константа меньше десяти");
+  
+}
+func(7, "Константа меньше десяти");*/
+
+/*function func() {
+  alert("Привет мир");
+}
+func();*/
+
+/*function func() {
+  console.log("javascript");
+}
+func();*/
+
+function func(x, y) {
+  let text = "lorem";
+  if (x < 9) {
+    console.log("У вас ошибка в тексте");
+  } else {
+    console.log("lorem");
+  }
+}
+func(1, 9);
